015662f9d61221ff6c6f08ac6b4e311cbd95cad1
Cassio <desenvolvedor.cassio@gmail.com>
2018-09-19 15:46:49 -0400

    working with RainTpl

--- index.php
+++ index.php
@@ -3,18 +3,19 @@
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
+use \Nortesoft\Page;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+
+	$page = new Page();
+
+	$page->setTpl("index");
     
-	$sql = new Nortesoft\DB\Sql();
-
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	
 });
 
 $app->run();
---
+++ views-cache/header.db0e11a150dc41559bbe21b0a2b45cab.rtpl.php
@@ -0,0 +1,9 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+		<title>Plataforma</title>
+	</head>
+	<body>
+	
+		
