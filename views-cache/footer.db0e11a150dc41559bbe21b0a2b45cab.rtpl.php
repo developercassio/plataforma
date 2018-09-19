f4f80045bc97a186089b6d36ef1855e197309f97
Cassio <desenvolvedor.cassio@gmail.com>
2018-09-19 16:08:02 -0400

    tpl site

--- views-cache/footer.db0e11a150dc41559bbe21b0a2b45cab.rtpl.php
+++ views-cache/footer.db0e11a150dc41559bbe21b0a2b45cab.rtpl.php
@@ -1,2 +1,19 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>	</body>
+<?php if(!class_exists('Rain\Tpl')){exit;}?>    <!-- CONTENT-WRAPPER SECTION END-->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2015 YourCompany | By : <a href="http://www.designbootstrap.com/" target="_blank">DesignBootstrap</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY SCRIPTS -->
+    <script src="res/site/assets/js/jquery-1.11.1.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="res/site/assets/js/bootstrap.js"></script>
+</body>
 </html>
