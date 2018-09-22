bbc9b6deeb36f44c2614ff203b83524600d7e160
Cassio <desenvolvedor.cassio@gmail.com>
2018-09-22 09:32:14 -0400

    Rotas para Admin e tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Nortesoft\Page;
+use \Nortesoft\Model\User;
+use \Nortesoft\PageAdmin;
 
 $app = new Slim();
 
@@ -17,6 +19,40 @@ $app->get('/', function() {
     
 	
 });
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+    
+	
+});
+
+
+/*$app->get('/login', function() {
+
+	$pageLogin = new Page([
+		"header" => "false",
+		"footer" => "false"
+	]);
+
+	$pageLogin->setTpl("login");
+});
+
+$app->post('/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header(location("Location: index"));
+
+	$pageLogin = new Page([
+		"header" => "false",
+		"footer" => "false"
+	]);
+
+	$pageLogin->setTpl("login");
+});*/
 
 $app->run();
 
--- vendor/nortesoft/php-classes/src/page.php
+++ vendor/nortesoft/php-classes/src/page.php
@@ -11,16 +11,18 @@ class Page {
 	private $options = [];
 
 	private $defauts = [
+		"header" => true,
+		"footer" => true,
 		"data" => []
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir = "/views/"){
 
 		$this->options = array_merge($this->defauts, $opts);
 
 		// config
 	$config = array(
-					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 					"debug"         => false // set to false to improve the speed
 				   );
@@ -32,7 +34,7 @@ class Page {
 
 	$this->setData($this->options['data']);
 
-	$this->tpl->draw("header");
+	if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -54,7 +56,7 @@ class Page {
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+	if ($this->options["footer"] === true)  $this->tpl->draw("footer");
 
 	}
 
