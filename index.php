8d33787292f564572d947daed4805e03687c130d
Cassio <desenvolvedor.cassio@gmail.com>
2018-09-23 18:47:01 -0400

    tela de login Admin

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -22,11 +22,23 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
     
 	
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+		]);
+
+	$page->setTpl("login");
 });
 
 
@@ -38,21 +50,24 @@ $app->get('/admin', function() {
 	]);
 
 	$pageLogin->setTpl("login");
-});
+});*/
 
-$app->post('/login', function() {
+$app->post('/admin/login', function() {
 
 	User::login($_POST["login"], $_POST["password"]);
 
-	header(location("Location: index"));
+	header("Location: /admin");
+	exit;
 
-	$pageLogin = new Page([
-		"header" => "false",
-		"footer" => "false"
-	]);
+});
 
-	$pageLogin->setTpl("login");
-});*/
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
 
 $app->run();
 
